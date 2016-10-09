c8b38bc5f23fe78b0cb5ebc101d6f6a00b91a820
Michal Ssamujlo <michal.samujlo@gmail.com>
2016-10-09 13:41:06 +0200

    rename TransactionTimeTest

--- src/test/java/com/infoshare/junit/$2_test_fixture/AllTests.java
+++ src/test/java/com/infoshare/junit/$2_test_fixture/AllTests.java
@@ -1,12 +1,11 @@
 package com.infoshare.junit.$2_test_fixture;
 
-import com.infoshare.junit.$1_first_test.CarUnitTest;
 import com.infoshare.junit.$3_basic_asserts.NewAccountTest;
 import com.infoshare.junit.$3_basic_asserts.TransactionTest;
 import com.infoshare.junit.$4_matchers.NewAccountMatchersTest;
 import com.infoshare.junit.$4_matchers.TransactionMatchersTest;
 import com.infoshare.junit.$5_assertj.TransferTest;
-import com.infoshare.junit.$6_stubs.TransactionTimeTest;
+import com.infoshare.junit.$6_stubs.TransactionStubTest;
 import com.infoshare.junit.$6_stubs.TransferBankTest;
 import com.infoshare.junit.$7_mocks.BankTest;
 import org.junit.runner.RunWith;
@@ -21,7 +20,7 @@ import org.junit.runners.Suite;
         NewAccountMatchersTest.class,
         TransactionMatchersTest.class,
         TransferTest.class,
-        TransactionTimeTest.class,
+        TransactionStubTest.class,
         TransferBankTest.class,
         BankTest.class
 })
--- src/test/java/com/infoshare/junit/$6_stubs/TransactionStubTest.java
+++ src/test/java/com/infoshare/junit/$6_stubs/TransactionStubTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class TransactionTimeTest {
+public class TransactionStubTest {
 
     @Test
     public void stub_transaction_amount() {
